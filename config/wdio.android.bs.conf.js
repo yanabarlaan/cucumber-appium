8c5241fb8c25c1cff900437dae0304d963c5ed70
yanabarlaan <yanabarlaan@gmail.com>
2022-07-01 18:26:13 +0800

    remove unnecessary file

--- config/wdio.android.bs.conf.js
+++ config/wdio.android.bs.conf.js
@@ -1,5 +1,5 @@
-require('dotenv').config();
-const {config} = require('./wdio.shared.conf');
+require("dotenv").config();
+const { config } = require("./wdio.shared.conf");
 //
 // ============
 // Browserstack
@@ -11,9 +11,7 @@ config.key = process.env.BROWSERSTACK_KEY;
 // ============
 // Specs
 // ============
-config.specs = [
-  './test/spec/login*.feature'
-];
+config.specs = ["./test/spec/*.feature"];
 
 //
 // ============
@@ -21,20 +19,18 @@ config.specs = [
 // ============
 config.capabilities = [
   {
-    platformName: 'Android',
-    'appium:platformVersion': "10.0",
-    'appium:deviceName': 'Google Pixel 3',
-    'appium:automationName': 'UIAutomator2',
-    'appium:app': "bs://9966fbcd19371acc9e5028b9a431d7aeefb0674d",
-    'appium:autoGrantPermissions': true
-  }
-]
+    platformName: "Android",
+    "appium:platformVersion": "10.0",
+    "appium:deviceName": "Google Pixel 3",
+    "appium:automationName": "UIAutomator2",
+    "appium:app": "bs://9966fbcd19371acc9e5028b9a431d7aeefb0674d",
+    "appium:autoGrantPermissions": true,
+  },
+];
 
 //
 // Test runner services
 // Services take over a specific job you don't want to take care of. They enhance
 // your test setup with almost no effort. Unlike plugins, they don't add new
 // commands. Instead, they hook themselves up into the test process.
-config.services = ['browserstack'],
-
-exports.config = config;
+(config.services = ["browserstack"]), (exports.config = config);
